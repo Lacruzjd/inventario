dbd4be8f8df4d1909f3cba4bb1cacccb62b39a95
lacruzjd <lacruzjd@gmail.com>
2022-07-01 15:50:43 -0300

    manejodatos

--- src/datos/ManejoDatos.java
+++ src/datos/ManejoDatos.java
@@ -1,14 +1,10 @@
 package datos;
 
-import java.util.ArrayList;
-
 public interface ManejoDatos {
     
-    public static final ArrayList<Object> DATOS = new ArrayList<>();
-
     public abstract void crear(Object elemento);
     public abstract void actualizar();
-    public abstract void eliminar();
+    public abstract void eliminar(Object elemento);
     public abstract void listar();
 
 }
